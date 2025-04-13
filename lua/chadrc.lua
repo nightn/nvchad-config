98c65a8444c889bdaf181e802a9f3e45e33f09c6
nightn <nightn@126.com>
2025-04-13 21:58:43 +0800

    Adapt new version of nvchad ui, we fix 'base46' nil value error

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,22 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+-- See :h nvui for more information
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
+M.base46 = {
   theme = "darcula",
+  theme_toggle = { "darcula", "github_light" },
 
+  -- hl_override = {
+  --   Comment = { italic = true },
+  --   ["@comment"] = { italic = true },
+  -- },
+}
+
+M.ui = {
   statusline = {
     separator_style = "round",
   },
@@ -14,23 +24,33 @@ M.ui = {
   tabufline = {
     lazyload = false,
   },
+}
 
-  nvdash = {
-    load_on_startup = true,
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f g", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
+M.nvdash = {
+  load_on_startup = true,
+  -- use the default nvdash.header in https://github.com/NvChad/ui
+
+  buttons = {
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fg", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
     },
-  }
 
-  -- hl_override = {
-  -- Comment = { italic = true },
-  -- ["@comment"] = { italic = true },
-  -- },
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+  },
 }
 
 return M
