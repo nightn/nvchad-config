487f8c4aa10a3901d2dc48fc19a3f155dceb6aa5
nightn <nightn@126.com>
2026-01-21 22:06:21 +0800

    Disable copilot auto trigger by default, add gn-language-server

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,6 +34,7 @@ return {
         "bash-language-server", -- bash
         "jedi-language-server", -- python
         "clang-format",
+        "gn-language-server", -- gn build system
       },
     },
   },
@@ -120,7 +121,7 @@ return {
     config = function()
       require("copilot").setup({
         suggestion = {
-          auto_trigger = true,
+          auto_trigger = false,  -- disable auto trigger by default
         },
         keymaps = {
           accept = "<M-l>",
@@ -139,5 +140,31 @@ return {
     },
     opts = {},
     event = "VeryLazy",
-  }
+  },
+
+  -- claude code
+  -- {
+  --   "coder/claudecode.nvim",
+  --   dependencies = { "folke/snacks.nvim" },
+  --   config = true,
+  --   keys = {
+  --     { "<leader>a", nil, desc = "AI/Claude Code" },
+  --     { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+  --     { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+  --     { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+  --     { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+  --     { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+  --     { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+  --     { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+  --     {
+  --       "<leader>as",
+  --       "<cmd>ClaudeCodeTreeAdd<cr>",
+  --       desc = "Add file",
+  --       ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+  --     },
+  --     -- Diff management
+  --     { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+  --     { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  --   },
+  -- },
 }
