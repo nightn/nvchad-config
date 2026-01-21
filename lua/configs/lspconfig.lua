eb2bd626dbe304d53950ad94d900c3da55d2d9a6
nightn <nightn@126.com>
2026-01-21 21:55:01 +0800

    Use new vim.lsp.config API for Neovim 0.11+

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,45 +1,42 @@
--- EXAMPLE
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+require("nvchad.configs.lspconfig").defaults()
 
 -- if you just want default config for the servers then put them in a table
 -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local lspconfig = require("lspconfig")
+-- read :h vim.lsp.config for changing options of lsp servers
 local servers = {
   -- <lsp server name>, <Mason plugin name>
   "html",
   "cssls",
   "bashls", -- bash-language-server
   "cmake", -- cmake-language-server
-  "clangd", -- clangd
+  -- "clangd", -- clangd
   "jedi_language_server", -- jedi-language-server
   -- vue support. required: npm install -g @vue/language-server
-  "volar", -- vue-language-server
+  "vue_ls", -- vue-language-server
+  "ts_ls", -- typescript-language-server
+  "gn_language_server", -- gn-language-server
 }
+-- Use new vim.lsp.config API for Neovim 0.11+
+vim.lsp.enable(servers)
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  })
-end
-
--- typescript
-lspconfig.ts_ls.setup({
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+local custom_clangd_query_driver = ""
+vim.lsp.config("clangd", {
+  cmd = {
+    "clangd",
+    -- Allow clangd to access these compilers
+    "--query-driver=/usr/bin/clang++,/usr/bin/g++,/usr/bin/aarch64-linux-gnu-g++," .. custom_clangd_query_driver,
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=never",
+  },
 })
+vim.lsp.enable("clangd")
 
 -- sourcekit
-lspconfig.sourcekit.setup({
+vim.lsp.config("sourcekit", {
   -- the default filetypes of sourcekit contains c/cpp, we should redefine it
   -- see: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/sourcekit.lua
   filetypes = { "swift", "objective-c", "objective-cpp" },
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-})
+});
+vim.lsp.enable("sourcekit")
+
